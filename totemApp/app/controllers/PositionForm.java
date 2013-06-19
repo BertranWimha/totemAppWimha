79cabdf95beb3800c46ad4f63d6ca27ed6b89db4
SimonWimha <sim.vacher@gmail.com>
2013-06-19 22:35:39 +0200

    Formulaire

---
+++ totemApp/app/controllers/PositionForm.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import play.data.validation.Constraints.Required;
+import play.i18n.Messages;
+import play.i18n.Lang;
+import play.Logger;
+
+
+public class PositionForm {
+
+	@Required
+	public String mail;
+	public String someText;
+
+
+	public PositionForm() {
+	}
+
+
+}
