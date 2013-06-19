7ce14770fecd66cebc8316393b9c8eaf6b7872f6
Simon Vacher <sim.vacher@gmail.com>
2013-06-19 23:20:27 +0200

    Design TODO display the latitude and longitude

--- totemApp/app/assets/javascript/getLocation.js
+++ totemApp/app/assets/javascript/getLocation.js
@@ -1,8 +1,9 @@
 if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(function(position){
-          var latitude = position.coords.latitude;
-          var longitude = position.coords.longitude;
-          var altitude = position.coords.altitude;
-          document.getElementById('geolocation').innerHTML = 'latitude : ' + latitude + '<br />' + 'longitude : ' + longitude + '<br />' + 'altitude : ' + altitude + '<br />';
-      });
-  }
+  navigator.geolocation.getCurrentPosition(function(position){
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
+      var altitude = position.coords.altitude;
+      //document.getElementById('geolocation').innerHTML = 'latitude : ' + latitude + '<br />' + 'longitude : ' + longitude + '<br />' + 'altitude : ' + altitude + '<br />';
+  	$('#geolocation').innerHTML(latitude + longitude); 
+  });
+}
